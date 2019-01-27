a5c8a977cf489881a81f60c9ff208f76d0f9ca06
Jarosław Rzeszótko <jrzeszotko@gmail.com>
2019-01-27 20:14:04 +0100

    Replace stow with rcm.
    (Working with the repo with all the files hidden was cumbersome,
    with tools like fd ignoring them by default)

--- install.sh
+++ install.sh
@@ -3,17 +3,6 @@
 # Fail on errors
 set -euxo pipefail
 
-# Dotfiles list
-DOTFILES=(
-    emacs
-    git
-    mysql
-    readline
-    tmux
-    x
-    zsh
-)
-
 # Change the shell
 SHELL="/bin/zsh"
 CURRENT_SHELL=$(getent passwd $LOGNAME | cut -d: -f7)
@@ -24,10 +13,5 @@ fi
 # Init the history file
 touch ~/.history
 
-# Disable ssh component of gnome keyring
-# (annoying modal window for ssh pass phrases)
-mkdir -p ~/.config/autostart
-cp config/autostart/gnome-keyring-ssh.desktop ~/.config/autostart
-
 # Install dotfiles
-stow -v -v -v -d . -t ~ ${DOTFILES[@]}
+rcup -d dotfiles
