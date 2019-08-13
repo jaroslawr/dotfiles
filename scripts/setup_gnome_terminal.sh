b4a0f2ca613b641f58a093a2ea2229416f93f554
Jarosław Rzeszótko <jrzeszotko@gmail.com>
2019-08-13 19:12:54 +0200

    Try to make gnome terminal setup script more reliable.

--- scripts/setup_gnome_terminal.sh
+++ scripts/setup_gnome_terminal.sh
@@ -2,8 +2,8 @@
 # Configure Gnome Terminal
 set -eux
 
-PROFILE_ID=$(dconf list /org/gnome/terminal/legacy/profiles:/)
-PROFILE_KEY="org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/${PROFILE_ID}"
+PROFILE_ID=$(dconf read /org/gnome/terminal/legacy/profiles:/default | sed s/\'//g)
+PROFILE_KEY="org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:${PROFILE_ID}/"
 
 function setting() {
   gsettings set "${PROFILE_KEY}" "${1}" "${2}"
@@ -13,6 +13,7 @@ setting "allow-bold" "false"
 setting "cell-height-scale" "1.1"
 setting "use-system-font" "false"
 setting "font" "Ubuntu Mono 12"
+setting "use-theme-colors" "false"
 setting "background-color" "#151515"
 setting "foreground-color" "#e2e4e5"
 setting "palette" "['#151515', '#ff5c57', '#5af78e', '#f3f99d', '#57c7ff', '#ff6ac1', '#9aedfe', '#e2e4e5', '#78787e', '#ff5c57', '#5af78e', '#f3f99d', '#57c7ff', '#ff6ac1', '#9aedfe', '#f1f1f0']"
