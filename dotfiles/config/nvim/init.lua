07f52f019654e6d481ad573950c8250a7b851310
Jarosław Rzeszótko <jrzeszotko@gmail.com>
2023-01-28 20:08:28 +0100

    Neovim - add some basic key bindings.

--- dotfiles/config/nvim/init.lua
+++ dotfiles/config/nvim/init.lua
@@ -20,3 +20,20 @@ opt.grepprg = 'rg -n $*'
 
 -- Show line numbers
 opt.number = true
+
+-- KEY BINDINGS
+--
+
+local keymap = vim.api.nvim_set_keymap
+local keymap_options = { noremap = true }
+
+-- leader
+vim.g.mapleader = ' '
+
+-- leader - general
+keymap('n', '<leader>x', ':qall!<cr>', keymap_options)
+keymap('n', '<leader>w', ':w<cr>', keymap_options)
+
+-- leader - fzf
+keymap('n', '<leader>fb', ':FzfLua buffers<cr>', keymap_options)
+keymap('n', '<leader>ff', ':FzfLua files<cr>', keymap_options)
