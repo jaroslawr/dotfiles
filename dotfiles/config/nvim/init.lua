f32e4596bbc3ac9b01bfea506199a7921e9ff4bd
Jarosław Rzeszótko <jrzeszotko@gmail.com>
2023-01-28 19:57:07 +0100

    Neovim - set some more basic options.

--- dotfiles/config/nvim/init.lua
+++ dotfiles/config/nvim/init.lua
@@ -3,8 +3,20 @@
 
 local opt = vim.opt
 
--- OPTIONS
+-- GENERAL OPTIONS
 --
+
+-- Enable mouse support
+opt.mouse = 'a'
 
 -- Use system clipboard
 opt.clipboard = 'unnamedplus'
+
+-- Grep with ripgrep
+opt.grepprg = 'rg -n $*'
+
+-- APPEARANCE
+--
+
+-- Show line numbers
+opt.number = true
