3ec92efdae4544c0ee922a36c15ab5df7cc2e16a
Jarosław Rzeszótko <jrzeszotko@gmail.com>
2023-02-04 19:42:07 +0100

    IPython - add config.

---
+++ dotfiles/ipython/profile_default/ipython_config.py
@@ -0,0 +1,13 @@
+c = get_config()
+
+# Shell color scheme
+c.TerminalInteractiveShell.colors = 'NoColor'
+
+# Syntax highlighting color scheme
+c.TerminalInteractiveShell.highlighting_style="one-dark"
+
+# Disable autosuggestion (that uses history items)
+c.TerminalInteractiveShell.autosuggestions_provider = None
+
+# Do not confirm exit, just exit
+c.TerminalInteractiveShell.confirm_exit = False
